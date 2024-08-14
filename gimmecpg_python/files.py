fb65818047f2d54c14a4c1c72c9acfb11abb59b9
niuums <niuzhengchai@gmail.com>
2024-08-14 12:59:19 +0100

    Read in IHEC csv

--- gimmecpg_python/files.py
+++ gimmecpg_python/files.py
@@ -43,65 +43,21 @@ def collapse_strands(bed):
 
 def read_files(file, mincov, collapse):
     """Scan files."""
+    print(f"not using 'mincov = {mincov} and collapse = {collapse}' options")
     name = Path(file).stem
     print(f"Scanning {name}")
     bed = (
         pl.scan_csv(
             file,
-            separator="\t",
-            skip_rows=1,
-            has_header=False,
-            dtypes={
-                "column_1": pl.Utf8,
-                "column_2": pl.UInt64,
-                "column_3": pl.UInt64,
-                "column_6": pl.Utf8,
-                "column_10": pl.UInt64,
-                "column_11": pl.UInt64,
-            },
+            separator=",", # convert to "," for actual files
+            has_header=True
         )  # cannot use scan() on zipped file
-        .select(
-            ["column_1", "column_2", "column_3", "column_6", "column_10", "column_11"]
-        )  # only select relevant columns
-        .rename(
-            {
-                "column_1": "chr",
-                "column_2": "start",
-                "column_3": "end",
-                "column_6": "strand",
-                "column_10": "coverage",
-                "column_11": "percent_methylated",
-            }
-        )  # rename to something that makes more sense
-        .with_columns(
-            pl.col("chr").str.replace_all(r"(?i)Chr", "")  # remove "chr" from Chr column to match reference
-        )
     )
 
-    if collapse:
-        data = collapse_strands(bed)
-        data = data.with_columns(
-            maxQuant=pl.col("total_coverage").quantile(0.999, "nearest")
-        )  # calculate max coverage quantile
-    else:
-        data = bed.with_columns(
-            pl.col("percent_methylated").alias("avg"),
-            pl.col("coverage").alias("total_coverage"),
-            maxQuant=pl.col("total_coverage").quantile(0.999, "nearest"),  # calculate max coverage quantile
-        )
+    bed_df = bed.melt(id_vars="l").collect().partition_by("variable", maintain_order = True)
+    bed_lf = [pl.LazyFrame(df) for df in bed_df]
 
-    quants = data.with_columns(
-        over=pl.col("total_coverage") - pl.col("maxQuant")
-    )  # identify rows that go over 99 quantile
-
-    data_cov_filt = (
-        quants.filter((pl.col("total_coverage") >= mincov) & (pl.col("over") < 0))  # filter by coverage
-        .with_columns(pl.lit(name).alias("sample"))
-        .select(["chr", "start", "strand", "avg", "sample"])
-        .cast({"chr": pl.Utf8, "start": pl.UInt64, "avg": pl.Float64, "sample": pl.Utf8})
-    )
-
-    return data_cov_filt
+    return bed_lf
 
 
 def save_files(df, outpath):
